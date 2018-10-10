8b3baba7df343508d7ded93dca78709cd162078f
Sean Langton <slangton1@gmail.com>
2018-10-10 01:39:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,9 @@
 cturn = 0
 def turn (board)
+until cturn == 1
 puts "Please enter 1-9:"
 input = gets.strip
 index = input_to_index(input)
-until cturn == 1
 if valid_move?(board,index) == true then
   move(board,index,"X")
   display_board(board)
